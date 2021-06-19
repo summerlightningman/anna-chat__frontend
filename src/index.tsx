e66488f7bd8b1670d12dc1b2c7f08dd3c71a805a
summerlightingman <div8@bk.ru>
2021-06-19 03:10:16 +0300

    TypedUseSelectorHook was added

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
+
 import './index.css';
 import App from './components/app/App';
-import {Provider} from "react-redux";
+
 import {store} from "./store";
 
 ReactDOM.render(
