2d207c0f370176cb2fe4979ba37113dc05cc44c1
summerlightingman <div8@bk.ru>
2021-07-18 16:48:40 +0300

    fix logout bug

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,11 +1,20 @@
-import {FC} from 'react';
+import {FC, useContext} from 'react';
 
 import Chat from "./chat/chat";
 import Header from "./header/header";
 
 import './main.css';
+import {Context} from "../../index";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {Redirect} from "react-router-dom";
 
 const Main: FC = () => {
+    const {auth} = useContext(Context);
+    const [user] = useAuthState(auth);
+
+    if (!user)
+        return <Redirect to="/login"/>
+
     return (
         <div className="chat-window">
             <Header/>
